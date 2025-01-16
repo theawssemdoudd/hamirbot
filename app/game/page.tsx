4db58b28f1e67db008588c662efefe49d4238968
theawssemdoudd <143346166+theawssemdoudd@users.noreply.github.com>
2025-01-16 20:36:58 +0100

    Update page.tsx

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,8 +1,10 @@
+import BottomNavigation from '@/components/BottomNavigation';  // استيراد المكون
 export default function Tasks() {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Tasks Page</h1>
       <p>Here are your games.</p>
+      <BottomNavigation />
     </div>
   );
 }
