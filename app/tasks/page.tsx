76c93a4716abf9f2557619d6ce5506a955f2581c
theawssemdoudd <143346166+theawssemdoudd@users.noreply.github.com>
2025-01-16 14:46:26 +0100

    Update page.tsx

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -3,6 +3,7 @@ export default function Tasks() {
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Tasks Page</h1>
       <p>Here are your games.</p>
+      <BottomNavigation />
     </div>
   );
 }
