6037a1ba0c03907c7903f323f58b8cb277926c02
theawssemdoudd <143346166+theawssemdoudd@users.noreply.github.com>
2025-01-16 01:07:52 +0100

    Update BottomNavigation.tsx

--- components/BottomNavigation.tsx
+++ components/BottomNavigation.tsx
@@ -3,11 +3,15 @@ import Link from 'next/link';
 export default function BottomNavigation() {
   return (
     <div className="fixed bottom-0 left-0 w-full bg-blue-500 p-4 text-white flex justify-around">
-      <Link href="/" className="text-center">Home</Link>
-      <Link href="/game" className="text-center">Game</Link> {/* الرابط إلى صفحة Game */}
-      <Link href="/tasks" className="text-center">Tasks</Link> {/* الرابط إلى صفحة Tasks */}
-      <Link href="/wallet" className="text-center">Wallet</Link>
-
+      <Link href="/" passHref>
+        <a className="text-center">Home</a>
+      </Link>
+      <Link href="/tasks" passHref>
+        <a className="text-center">Tasks</a> {/* الرابط إلى صفحة Tasks */}
+      </Link>
+      <Link href="/wallet" passHref>
+        <a className="text-center">Wallet</a>
+      </Link>
     </div>
   );
 }
