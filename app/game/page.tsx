c02de80bd09ac083657ef1e0d67d304c7c563f22
theawssemdoudd <143346166+theawssemdoudd@users.noreply.github.com>
2025-01-16 11:09:14 +0100

    Create page.tsx

---
+++ app/game/page.tsx
@@ -0,0 +1,8 @@
+export default function Tasks() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Tasks Page</h1>
+      <p>Here are your games.</p>
+    </div>
+  );
+}
