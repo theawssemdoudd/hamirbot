172637d8e5e4a7384a11e7df62fcc5be44e33f96
theawssemdoudd <143346166+theawssemdoudd@users.noreply.github.com>
2025-01-17 23:00:48 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,19 @@
-import type { Metadata } from "next";
+'use client'
+
 import "./globals.css";
-import { Inter } from "next/font/google";
-import Script from "next/script";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Telegram Mini App",
-  description: "A simple Telegram mini app using Next.js and Prisma",
-};
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
       <head>
-        {/* تحويل العنوان إلى نص صريح */}
-        <title></title>
+        <title>TON Connect Demo</title>
       </head>
       <body>
-        <Script
-          src="https://telegram.org/js/telegram-web-app.js"
-          strategy="beforeInteractive"
-        />
-        {/* تضمين TonConnect */}
         <TonConnectUIProvider manifestUrl="https://violet-traditional-rabbit-103.mypinata.cloud/ipfs/QmQJJAdZ2qSwdepvb5evJq7soEBueFenHLX3PoM6tiBffm">
           {children}
         </TonConnectUIProvider>
