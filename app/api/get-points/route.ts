aa210721cefe0b0eaeaf744ee94a490a40095270
theawssemdoudd <143346166+theawssemdoudd@users.noreply.github.com>
2025-01-18 01:15:42 +0100

    Update route.ts

--- app/api/get-points/route.ts
+++ app/api/get-points/route.ts
@@ -29,3 +29,4 @@ export async function POST(req: NextRequest) {
         console.error('Error increasing points:', error)
         return NextResponse.json({ error: 'Internal server error' }, { status: 500 })
     }
+
